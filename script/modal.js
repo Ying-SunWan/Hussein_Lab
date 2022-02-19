d1573a260e6ab675d0b46b2264cef6300cea12b9
Nehal Sekhon <nehalsekhon@gmail.com>
2022-02-18 18:27:50 -0700

    add team popup

--- script/modal.js
+++ script/modal.js
@@ -1,6 +1,12 @@
 function popoutbio(button) {
     var x = button.id;
-    console.log(x);
     bios = document.getElementsByClassName("bio");
     bios[x].style.visibility = "visible";
+}
+
+function exit() {
+    bios = document.getElementsByClassName("bio");
+    for (let i = 0; i < bios.length; i++) {
+        bios[i].style.visibility = "hidden";
+    }
 }
