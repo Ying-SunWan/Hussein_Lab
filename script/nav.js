17bb902f555dd8fc1d86dd6a8df320b0352a60a4
Nehal Sekhon <nehalsekhon@gmail.com>
2022-05-10 23:06:36 -0600

    make mobile friendly

--- script/nav.js
+++ script/nav.js
@@ -1,12 +1,21 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll < 400) {
-          $(".sidenav a").css("color" , "white");
-        }
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll < 400) {
+//           $(".sidenav a").css("color" , "white");
+//         }
    
-        else{
-            $(".sidenav a").css("color" , "black");  	
-        }
-    })
-  })
+//         else{
+//             $(".sidenav a").css("color" , "black");  	
+//         }
+//     })
+//   })
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navbar = document.getElementsByClassName('navbar')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+  navbar.classList.toggle('active')
+})
